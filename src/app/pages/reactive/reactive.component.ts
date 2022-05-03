4a5d36f7ebcab262dd2048b2efbe2472ac9d2a46
xxavierargentino <xxavierargentino@hotmail.com>
2022-05-03 19:39:58 -0300

    agregando campo anidado a formulario reactivo

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -21,14 +21,36 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario(){
     this.forma = this.fb.group({
-        nombre  : ['Fernando Javier'],
-        apellido: ['Salva'],
-        correo  : ['xxavierargentino@hotmail.com']
+      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      nombre  : ['',[Validators.required, Validators.minLength(5)]],
+      correo  : ['', [Validators.required, Validators.minLength(5), Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]],
+      direccion: this.fb.group({
+        provincia: ['', Validators.required],
+        ciudad: ['', Validators.required],
+      })
     });
+  }
+
+  get nombreNoValido() {
+    return this.forma.get('nombre')?.invalid && this.forma.get('nombre')?.touched;
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido')?.invalid && this.forma.get('apellido')?.touched;
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched;
   }
 
   guardar(){
     console.log(this.forma);
+
+    if(this.forma.invalid){
+      return Object.values(this.forma.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
   }
 
 }
